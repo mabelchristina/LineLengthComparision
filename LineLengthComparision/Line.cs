5269d09d7b067bbeef071864527cc039a3b7d3a0
Mabel <maybelchristina@gmail.com>
2021-09-20 22:16:27 +0530

    [Mabel] Add: UC1 Added

---
+++ LineLengthComparision/Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineLengthComparision
+{
+    class Line
+    {
+        public double Length(double x1, double x2, double y1, double y2)
+        {
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return length;
+        }
+    }
+}
---
+++ LineLengthComparision/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineLengthComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = new Line().Length(4, 2, 4, 2);
+            Console.WriteLine("Length of the line is:" + length);
+            Console.ReadLine();
+        }
+    }
+}
