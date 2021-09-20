1ae4f16c325e5180ab5c31be1d1e4ddfb0f294ee
Mabel <maybelchristina@gmail.com>
2021-09-20 22:50:51 +0530

    [Mabel] Add: UC3 added

--- LineLengthComparision/Program.cs
+++ LineLengthComparision/Program.cs
@@ -6,16 +6,21 @@ namespace LineLengthComparision
     {
         static void Main(string[] args)
         {
-            double length1 = new Line().Length(4, 2, 4, 2);
+            double length1 = new Line().Length(4, 4, 4, 4);
             double length2 = new Line().Length(4, 4, 4, 4);
-            if (length1.Equals(length2))
+            switch (length1.CompareTo(length2))
             {
-                Console.WriteLine("Both lengths are equal");
-            }
-            else
-            {
-                Console.WriteLine("lengths are not equal");
+                case 0:
+                    Console.WriteLine("Lengths are equal");
+                    break;
+                case 1:
+                    Console.WriteLine("Line 1 is greater");
+                    break;
+                default:
+                    Console.WriteLine("Line 2 is greater");
+                    break;
             }
         }
     }
 }
+
